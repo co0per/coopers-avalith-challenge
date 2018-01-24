6bc1bf97b62ffc43bbdb07cca2c3361a916f2f46
Lisandro Cooper <coopermegadeth@gmail.com>
2018-01-24 15:40:36 -0300

    Display cards and card searcher

--- src/components/viewHome.vue
+++ src/components/viewHome.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col l3 m5 s12">
+      <div class="col">
         <div class="gray-box">
           <label class="label-top lwhite">DEV NAME</label><br>
           <label class="label-mid lwhite">DNI XX.XXX.XXX</label><br>
@@ -9,7 +9,7 @@
         </div> 
         <div>
           <div class="input-field input-mar">
-            <input id="search" type="text" placeholder="Search in cards">
+            <input id="search" type="text" placeholder="Search in cards" v-model="search_filter">
           </div>
           <label class="lwhite mar-left-search label-mid">Filter by</label>
           <p class="mar-left-search">
@@ -22,9 +22,25 @@
           </p>
         </div>  
       </div>
-      <div class="col l9">
+      <div class="col">
         <div class="results">
           <h5>LANGUAGES & TECHNOLOGIES</h5>
+          <div class="row">
+            <div class="flex-container">
+              <div v-for="card_lt in lang_tech_search">
+                  <div class="">
+                    <div class="card small">
+                      <div class="card-image">
+                        <img :src="card_lt.cardImageUrl" class="card-img">
+                      </div>
+                      <div class="card-content">
+                        <p>{{ card_lt.cardDescription }}</p>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -32,28 +48,36 @@
 </template>
 
 <script>
-    export default {
-      name: 'viewHome',
-      components:{
-      }, 
-      data(){ 
-        return {
 
-        }
-      },
-      mounted(){
+  import json_card from './../cards.js';
 
-      },
-      created() {
+  export default {
+    name: 'viewHome',
+    components:{
+    }, 
+    data(){ 
+      return {
+        lang_tech_list: [],
+        search_filter: ''
+      }
+    },
+    mounted(){
+      this.lang_tech_list = json_card;
+    },
+    created() {
         
-      },
-      methods: {
+    },
+    methods: {
 
-      },
-      computed: {
-
+    },
+    computed: {
+      lang_tech_search() {
+        return this.lang_tech_list.filter(lt_list => {
+          return lt_list.cardTitle.toLowerCase().includes(this.search_filter.toLowerCase())
+        })
       }
     }
+  }
 </script>
 
 <style>
